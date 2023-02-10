809fee6781f67cbbe140908e530e7eced2e6bb30
MengbingZhou <71744383+Zhou-MB@users.noreply.github.com>
2023-02-10 15:40:34 +0800

    changeWeather

--- main.py
+++ main.py
@@ -20,10 +20,13 @@ template_id = os.environ["TEMPLATE_ID"]
 
 
 def get_weather():
-  url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + city
-  res = requests.get(url).json()
-  weather = res['data']['list'][0]
-  return weather['weather'], math.floor(weather['temp'])
+  try:
+    url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + city
+    res = requests.get(url).json()
+    weather = res['data']['list'][0]
+    return weather['weather'], math.floor(weather['temp'])
+  else:  
+    return "未知",404
 
 def get_count():
   delta = today - datetime.strptime(start_date, "%Y-%m-%d")
@@ -52,6 +55,6 @@ client = WeChatClient(app_id, app_secret)
 wm = WeChatMessage(client)
 wea, temperature = get_weather()
 data = {"weather":{"value":wea},"temperature":{"value":temperature},"love_days":{"value":get_count()},"birthday_left":{"value":get_birthday()},"words":{"value":get_words(), "color":get_random_color()}}
-#res = wm.send_template(user_id, template_id, data)
+res = wm.send_template(user_id, template_id, data)
 res2 = wm.send_template(user_id2, template_id, data)
-print(res2)
+print(res,res2)
